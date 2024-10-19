eaa1e177a28f19d420e42fc8ee82f804ba59db89
dmac <DaraMac@users.noreply.github.com>
2024-10-19 17:47:33 +0100

    Temporarily disable autologin since that's broken on gnome nixos

--- configuration.nix
+++ configuration.nix
@@ -134,7 +134,7 @@
     };
 
     # Enable automatic login for the user.
-    services.displayManager.autoLogin.enable = true;
+    services.displayManager.autoLogin.enable = false;
     services.displayManager.autoLogin.user = "dara";
 
     # Allow unfree packages
