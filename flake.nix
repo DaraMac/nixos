049ab26ecab338b9695f5b75ebd89cf8c34edd38
dmac <DaraMac@users.noreply.github.com>
2024-11-08 21:08:57 +0000

    Update flake and remove commented out cosmic stuff

--- flake.nix
+++ flake.nix
@@ -4,29 +4,16 @@
     inputs = {
         # NixOS official package source, using the unstable branch here
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-        # nixos-cosmic = {
-        #     url = "github:lilyinstarlight/nixos-cosmic";
-        #     inputs.nixpkgs.follows = "nixpkgs";
-        # };
     };
 
     outputs = { self, nixpkgs, ... }@inputs: {
-        # Please replace my-nixos with your hostname
+        # Your hostname here
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
                 # Import the previous configuration.nix we used,
                 # so the old configuration file still takes effect
                 ./configuration.nix
-
-                # {
-                #     nix.settings = {
-                #         substituters = [ "https://cosmic.cachix.org" ];
-                #         trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-                #     };
-                # }
-                # nixos-cosmic.nixosModules.default
             ];
         };
     };
