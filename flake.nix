b6562c11a8d9fc495918412e5d8f7bc85b4bf925
dmac <DaraMac@users.noreply.github.com>
2024-11-23 16:36:27 +0000

    Remove k-framework as not using at the moment

--- flake.nix
+++ flake.nix
@@ -11,8 +11,7 @@
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
-                # Import the previous configuration.nix we used,
-                # so the old configuration file still takes effect
+                # Import the original configuration file
                 ./configuration.nix
             ];
         };
