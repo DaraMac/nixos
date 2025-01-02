b69bf8a1c8c6fa3aea455c90088d15dc3ff70bd3
Dara <DaraMac@users.noreply.github.com>
2025-01-02 19:03:10 +0000

    Add home manager flake, get git working in it

--- flake.nix
+++ flake.nix
@@ -4,15 +4,23 @@
     inputs = {
         # NixOS official package source, using the unstable branch here
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	home-manager.url = "github:nix-community/home-manager";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = { self, nixpkgs, ... }@inputs: {
+    outputs = { self, nixpkgs, home-manager, ... }@inputs: {
         # Your hostname here
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
                 # Import the original configuration file
                 ./configuration.nix
+		home-manager.nixosModules.home-manager
+		{
+			home-manager.useGlobalPkgs = true;
+			home-manager.useUserPackages = true;
+			home-manager.users.dara = import ./home.nix;
+		}
             ];
         };
     };
---
+++ home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ...}:
+
+{	
+	# Home Manager needs a bit of information about you and the
+	# paths it should manage.
+	home.username = "dara";
+	home.homeDirectory = "/home/dara";
+
+	# This value determines the Home Manager release that your
+	# configuration is compatible with. This helps avoid breakage
+	# when a new Home Manager releases introduces backwards
+	# incompatible changes.
+	#
+	# You can update Home Manager without changing this value. See
+	# the Home Manager releases notes for a list of state version
+	# changes in each release.
+	home.stateVersion = "25.05";
+
+	programs.home-manager.enable = true;
+
+	programs.git = {
+		enable = true;
+		userEmail = "DaraMac@users.noreply.github.com";
+		userName  = "dmac";
+		extraConfig = {
+			core.editor = "nvim";
+			diff.algorithm = "histogram";
+			init.defaultBranch = "main";
+			log.date = "iso";
+			merge.conflictstyle = "diff3";
+			pull.rebase = "true";
+			push.autoSetupRemote = "true";
+		};
+	};
+}
