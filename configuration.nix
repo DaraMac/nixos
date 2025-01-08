9d6c05274611ce954d6ed0cdeb5b76b071ff486b
Dara <DaraMac@users.noreply.github.com>
2025-01-08 12:42:25 +0000

    Get p10k prompt working

--- configuration.nix
+++ configuration.nix
@@ -88,7 +88,7 @@
         enable = true;
         # autosuggestions.enable = true;
         # syntaxHighlighting.enable = true;
-        # promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
         # ohMyZsh = {
         #     enable = true;
         #     plugins = [
