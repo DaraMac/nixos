7ac4f039ba8163b7b18adc1b645dac04b9dc0c2e
Dara <DaraMac@users.noreply.github.com>
2025-01-09 13:28:26 +0000

    Finish off the git aliases and functions, format them a bit, escape some characters

--- home.nix
+++ home.nix
@@ -199,7 +199,88 @@
 			"gpf!"="git push --force";
 			gpf="git push --force-with-lease --force-if-includes";
 
-			# end git aliases
+			gpsup="git push --set-upstream origin $(git_current_branch)";
+			gpsupf="git push --set-upstream origin $(git_current_branch) --force-with-lease --force-if-includes";
+			gpv="git push --verbose";
+			gpoat="git push origin --all && git push origin --tags";
+			gpod="git push origin --delete";
+			ggpush=''git push origin "$(git_current_branch)"'';
+
+			gpu=''git push upstream'';
+				grb=''git rebase'';
+				grba=''git rebase --abort'';
+				grbc=''git rebase --continue'';
+				grbi=''git rebase --interactive'';
+				grbo=''git rebase --onto'';
+				grbs=''git rebase --skip'';
+				grbd=''git rebase $(git_develop_branch)'';
+				grbm=''git rebase $(git_main_branch)'';
+				grbom=''git rebase origin/$(git_main_branch)'';
+				grbum=''git rebase upstream/$(git_main_branch)'';
+				grf=''git reflog'';
+				gr=''git remote'';
+				grv=''git remote --verbose'';
+				gra=''git remote add'';
+				grrm=''git remote remove'';
+				grmv=''git remote rename'';
+				grset=''git remote set-url'';
+				grup=''git remote update'';
+				grh=''git reset'';
+				gru=''git reset --'';
+				grhh=''git reset --hard'';
+				grhk=''git reset --keep'';
+				grhs=''git reset --soft'';
+				gpristine=''git reset --hard && git clean --force -dfx'';
+				gwipe=''git reset --hard && git clean --force -df'';
+				groh=''git reset origin/$(git_current_branch) --hard'';
+				grs=''git restore'';
+				grss=''git restore --source'';
+				grst=''git restore --staged'';
+				gunwip=''git rev-list --max-count=1 --format="%s" HEAD | grep -q "\--wip--" && git reset HEAD~1'';
+				grev=''git revert'';
+				greva=''git revert --abort'';
+				grevc=''git revert --continue'';
+				grm=''git rm'';
+				grmc=''git rm --cached'';
+				gcount=''git shortlog --summary --numbered'';
+				gsh=''git show'';
+				gsps=''git show --pretty=short --show-signature'';
+				gstall=''git stash --all'';
+				gstaa=''git stash apply'';
+				gstc=''git stash clear'';
+				gstd=''git stash drop'';
+				gstl=''git stash list'';
+				gstp=''git stash pop'';
+				gsta=''git stash push'';
+				gsts=''git stash show --patch'';
+				gst=''git status'';
+				gss=''git status --short'';
+				gsb=''git status --short --branch'';
+				gsi=''git submodule init'';
+				gsu=''git submodule update'';
+				gsd=''git svn dcommit'';
+				git-svn-dcommit-push=''git svn dcommit && git push github $(git_main_branch):svntrunk'';
+				gsr=''git svn rebase'';
+				gsw=''git switch'';
+				gswc=''git switch --create'';
+				gswd=''git switch $(git_develop_branch)'';
+				gswm=''git switch $(git_main_branch)'';
+				gta=''git tag --annotate'';
+				gts=''git tag --sign'';
+				gtv=''git tag | sort -V'';
+				gignore=''git update-index --assume-unchanged'';
+				gunignore=''git update-index --no-assume-unchanged'';
+				gwch=''git whatchanged -p --abbrev-commit --pretty=medium'';
+				gwt=''git worktree'';
+				gwta=''git worktree add'';
+				gwtls=''git worktree list'';
+				gwtmv=''git worktree move'';
+				gwtrm=''git worktree remove'';
+				gstu=''gsta --include-untracked'';
+				gtl=''gtl(){ git tag --sort=-v:refname -n --list "''${1}*" }; noglob gtl'';
+				gk=''\gitk --all --branches &!'';
+				gke=''\gitk --all $(git log --walk-reflogs --pretty=%h) &!'';
+				# end git aliases
 		};
 
 		plugins = [
@@ -223,36 +304,65 @@
 		initExtra = ''
 		# start git functions
 
+		# Check for develop and similarly named branches
+		function git_develop_branch() {
+			command git rev-parse --git-dir &>/dev/null || return
+			local branch
+			for branch in dev devel develop development; do
+				if command git show-ref -q --verify refs/heads/$branch; then
+					echo $branch
+					return 0
+				fi
+			done
+
+			echo develop
+			return 1
+		}
+
+		function grename() {
+			if [[ -z "$1" || -z "$2" ]]; then
+				echo "Usage: $0 old_branch new_branch"
+				return 1
+			fi
+
+			# Rename branch locally
+			git branch -m "$1" "$2"
+			# Rename branch in origin remote
+			if git push origin :"$1"; then
+				git push --set-upstream origin "$2"
+			fi
+		}
+
 		function gbda() {
 			git branch --no-color --merged | command grep -vE "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)" | command xargs git branch --delete 2>/dev/null
 		}
 
-# https://github.com/jmaroeder/plugin-git/blob/216723ef4f9e8dde399661c39c80bdf73f4076c4/functions/gbda.fish
+		# https://github.com/jmaroeder/plugin-git/blob/216723ef4f9e8dde399661c39c80bdf73f4076c4/functions/gbda.fish
 		function gbds() {
 			local default_branch=$(git_main_branch)
-				(( ! $? )) || default_branch=$(git_develop_branch)
+			(( ! $? )) || default_branch=$(git_develop_branch)
 
-				git for-each-ref refs/heads/ "--format=%(refname:short)" | \
-				while read branch; do
-					local merge_base=$(git merge-base $default_branch $branch)
-						if [[ $(git cherry $default_branch $(git commit-tree $(git rev-parse $branch\^{tree}) -p $merge_base -m _)) = -* ]]; then
-							git branch -D $branch
-								fi
-								done
+			git for-each-ref refs/heads/ "--format=%(refname:short)" | \
+			while read branch; do
+				local merge_base=$(git merge-base $default_branch $branch)
+					if [[ $(git cherry $default_branch $(git commit-tree $(git rev-parse $branch\^{tree}) -p $merge_base -m _)) = -* ]]; then
+						git branch -D $branch
+					fi
+				done
 		}
 
 		function gccd() {
 			setopt localoptions extendedglob
 
-# get repo URI from args based on valid formats: https://git-scm.com/docs/git-clone#URLS
-				local repo="''${''${@[(r)(ssh://*|git://*|ftp(s)#://*|http(s)#://*|*@*)(.git/#)#]}:-$_}"
+			# get repo URI from args based on valid formats: https://git-scm.com/docs/git-clone#URLS
+			local repo="''${''${@[(r)(ssh://*|git://*|ftp(s)#://*|http(s)#://*|*@*)(.git/#)#]}:-$_}"
 
-# clone repository and exit if it fails
-				command git clone --recurse-submodules "$@" || return
+			# clone repository and exit if it fails
+			command git clone --recurse-submodules "$@" || return
 
-# if last arg passed was a directory, that's where the repo was cloned
-# otherwise parse the repo URI and use the last part as the directory
-				[[ -d "$_" ]] && cd "$_" || cd "''${''${repo:t}%.git/#}"
+			# if last arg passed was a directory, that's where the repo was cloned
+			# otherwise parse the repo URI and use the last part as the directory
+			[[ -d "$_" ]] && cd "$_" || cd "''${''${repo:t}%.git/#}"
 		}
 		compdef _git gccd=git-clone
 
@@ -299,6 +409,16 @@
 			git push --force-with-lease origin "''${b:=$1}"
 		}
 		compdef _git ggfl=git-checkout
+
+		function ggp() {
+			if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
+				git push origin "''${*}"
+			else
+				[[ "$#" == 0 ]] && local b="$(git_current_branch)"
+				git push origin "''${b:=$1}"
+			fi
+		}
+		compdef _git ggp=git-checkout
 
 		### end git functions
 
