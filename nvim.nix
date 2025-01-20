d03cdf8f0cd5a1a237c386e686965fead1223c85
Dara <DaraMac@users.noreply.github.com>
2025-01-20 16:56:36 +0000

    Add basic vim.o options to nvim

--- nvim.nix
+++ nvim.nix
@@ -15,6 +15,48 @@
                                 netrw_banner = 0;
                                 netrw_liststyle = 3;
                         };
+
+                        options = {
+                                guicursor = '''';
+
+                                number = true;
+                                relativenumber = true;
+                                signcolumn = ''number'';
+
+                                mouse = ''a'';
+
+                                # clipboard:append { 'unnamedplus' }
+
+                                breakindent = true;
+
+                                tabstop = 4;
+                                softtabstop = 4;
+                                shiftwidth = 4;
+                                expandtab = true;
+
+                                smartindent = true;
+
+                                termguicolors = true;
+
+                                ignorecase = true;
+                                smartcase = true;
+
+                                list = true;
+                                # listchars = 
+
+                                autowrite = true;
+
+                                cursorline = true;
+
+                                # setting it to a very large value just to keep it always in centre
+                                scrolloff = 999;
+
+                                hlsearch = true;
+
+                                ruler = false;
+
+                                # tags:append('./.tags;/')
+                        };
                 };
         };
 }
