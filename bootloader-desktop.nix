b1a240a2f5b95b4f3930db34c360804fd23f5c74
dmac <DaraMac@users.noreply.github.com>
2025-02-28 10:21:47 +0000

    Refactor settings in bootloader

--- bootloader-desktop.nix
+++ bootloader-desktop.nix
@@ -2,7 +2,13 @@
 
 {
     # Bootloader.
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/nvme0n1";
-    boot.loader.grub.useOSProber = true;
+    boot = {
+        loader = {
+            grub = {
+                device = "/dev/nvme0n1";
+                enable = true;
+                useOSProber = true;
+            };
+        };
+    };
 }
