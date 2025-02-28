ec25fa631392d927d68486d081ec79fbb814f5ae
dmac <DaraMac@users.noreply.github.com>
2025-02-28 09:35:28 +0000

    Add leeckblock to firefox, reorder extensions

--- firefox.nix
+++ firefox.nix
@@ -24,21 +24,21 @@
                 };
 
                 ExtensionSettings = {
-                    # uBlock Origin
-                    "uBlock0@raymondhill.net" = {
-                        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+                    # BetterTTV
+                    "firefox@betterttv.net" = {
+                        install_url = "https://addons.mozilla.org/firefox/downloads/latest/betterttv/latest.xpi";
+                        installation_mode = "force_installed";
+                    };
+
+                    # LeechBlock NG
+                    "leechblockng@proginosko.com" = {
+                        install_url = "https://addons.mozilla.org/firefox/downloads/latest/leechblock-ng/latest.xpi";
                         installation_mode = "force_installed";
                     };
 
                     # NoScript
                     "{73a6fe31-595d-460b-a920-fcc0f8843232}" = {
                         install_url = "https://addons.mozilla.org/firefox/downloads/latest/noscript/latest.xpi";
-                        installation_mode = "force_installed";
-                    };
-
-                    # BetterTTV
-                    "firefox@betterttv.net" = {
-                        install_url = "https://addons.mozilla.org/firefox/downloads/latest/betterttv/latest.xpi";
                         installation_mode = "force_installed";
                     };
 
@@ -51,6 +51,12 @@
                     # Tridactyl
                     "tridactyl.vim@cmcaine.co.uk" = {
                         install_url = "https://addons.mozilla.org/firefox/downloads/latest/tridactyl-vim/latest.xpi";
+                        installation_mode = "force_installed";
+                    };
+
+                    # uBlock Origin
+                    "uBlock0@raymondhill.net" = {
+                        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
                         installation_mode = "force_installed";
                     };
 
