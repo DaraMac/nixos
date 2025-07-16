4313b97cb994e96f28a79a32b54881a91f73646b
dmac <DaraMac@users.noreply.github.com>
2025-07-16 17:12:37 +0100

    Add stylix, update flake.lock

--- flake.nix
+++ flake.nix
@@ -6,18 +6,21 @@
             inputs.nixpkgs.follows = "nixpkgs";
             url = "github:nix-community/home-manager";
         };
+        stylix = {
+            url = "github:danth/stylix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
         # NixOS official package source, using the unstable branch here
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs: {
         # Your hostname here
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             modules = [
-                # Import the original configuration file
-                ./configuration.nix
-
+                ./configuration.nix # import the original configuration file
+                stylix.nixosModules.stylix
                 home-manager.nixosModules.home-manager
                 {
                     home-manager = {
