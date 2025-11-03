e0ccffb44c3e158fb5f5d8901d10efe2a25a15bc
dmac <DaraMac@users.noreply.github.com>
2025-11-03 18:41:28 +0000

    Remove stylix from flake

--- flake.nix
+++ flake.nix
@@ -6,21 +6,16 @@
             inputs.nixpkgs.follows = "nixpkgs";
             url = "github:nix-community/home-manager";
         };
-        stylix = {
-            url = "github:danth/stylix";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
 
         # NixOS official package source, using the unstable branch here
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs: {
+    outputs = { self, nixpkgs, home-manager, ... }@inputs: {
         # Your hostname here
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             modules = [
                 ./configuration.nix # import the original configuration file
-                stylix.nixosModules.stylix
                 home-manager.nixosModules.home-manager
                 {
                     home-manager = {
