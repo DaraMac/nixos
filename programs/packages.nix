ed6243911bd3bb68153a86ae541f583f1035fdac
dmac <DaraMac@users.noreply.github.com>
2025-12-07 03:24:55 +0000

    Try readest

--- programs/packages.nix
+++ programs/packages.nix
@@ -70,6 +70,7 @@
     pinentry-gnome3 # passwords
     playerctl # niri
     python3 # dev tools
+    readest # ebooks
     renameutils # cli
     resources # gnome circle system monitor
     ripgrep # cli
