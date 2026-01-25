5f7722a72f33a3f25e87f325ffdbda57e5bf86ce
dmac <DaraMac@users.noreply.github.com>
2026-01-25 18:31:29 +0000

    Try enable bluetooth for noctalia

--- configuration.nix
+++ configuration.nix
@@ -182,6 +182,8 @@
         ];
     };
 
+    hardware.bluetooth.enable = true; # noctalia
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
