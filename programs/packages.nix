e5bc503932b6ddf49148cec9a389cd19ef3a84fa
dmac <DaraMac@users.noreply.github.com>
2026-03-20 16:58:02 +0000

    Try out gapless again

--- programs/packages.nix
+++ programs/packages.nix
@@ -40,6 +40,7 @@
     flac # music, just for metaflac tag tool
     fluffychat # matrix messaging
     fragments # torrent
+    gapless # music player
     gimp3 # graphics
     git # dev tools
     gnome-boxes # vm
