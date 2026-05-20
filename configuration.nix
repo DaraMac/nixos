9ccb26f71547942f264f19dda164f7ab80d77c8a
dmac <DaraMac@users.noreply.github.com>
2026-05-20 04:01:59 +0100

    Consolidate extra locale settings

--- configuration.nix
+++ configuration.nix
@@ -37,18 +37,7 @@
     i18n = {
         defaultLocale = "it_IT.UTF-8";
         extraLocales = ["en_GB.UTF-8/UTF-8" "en_IE.UTF-8/UTF-8" "ga_IE.UTF-8/UTF-8"];
-
-        extraLocaleSettings = {
-            LC_ADDRESS = "en_IE.UTF-8";
-            LC_IDENTIFICATION = "en_IE.UTF-8";
-            LC_MEASUREMENT = "en_IE.UTF-8";
-            LC_MONETARY = "en_IE.UTF-8";
-            LC_NAME = "en_IE.UTF-8";
-            LC_NUMERIC = "en_IE.UTF-8";
-            LC_PAPER = "en_IE.UTF-8";
-            LC_TELEPHONE = "en_IE.UTF-8";
-            LC_TIME = "en_IE.UTF-8";
-        };
+        extraLocaleSettings.LC_ALL = "en_IE.UTF-8";
     };
 
     # PipeWire uses this to acquire realtime priority
